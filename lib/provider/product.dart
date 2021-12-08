f3f06132e6bc6aae6669d6aec2380bc339b56e9f
adi wirya <adi.wirya@student.umn.ac.id>
2021-12-08 17:30:27 +0700

    Lecture 200

--- lib/provider/product.dart
+++ lib/provider/product.dart
@@ -1,4 +1,5 @@
-class Product {
+import 'package:flutter/material.dart';
+class Product with ChangeNotifier {
   final String id;
   final String title;
   final String description;
@@ -14,4 +15,9 @@ class Product {
     required this.price,
     this.isFavorite = false,
   });
+
+  void toggleFavoriteStatus() {
+    isFavorite = !isFavorite;
+    notifyListeners();
+  }
 }
--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,41 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-import '../screens/product_detail.dart';
+import '../screens/product_detail_screen.dart';
+import '../provider/product.dart';
 
 class ProductItem extends StatelessWidget {
-  final String id;
-  final String title;
-  final String imageUrl;
+  // final String id;
+  // final String title;
+  // final String imageUrl;
 
-  ProductItem(this.id, this.title, this.imageUrl);
+  // ProductItem(this.id, this.title, this.imageUrl);
 
   @override
   Widget build(BuildContext context) {
+    final product = Provider.of<Product>(context, listen: false);
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
         child: GestureDetector(
           onTap: () {
-            Navigator.of(context).push(
-              MaterialPageRoute(
-                builder: (context) => ProductDetailScreen(title),
-              ),
+            Navigator.of(context).pushNamed(
+              ProductDetailScreen.routeName,
+              arguments: product.id,
             );
           },
           child: Image.network(
-            imageUrl,
+            product.imageUrl,
             fit: BoxFit.cover,
           ),
         ),
         footer: GridTileBar(
           backgroundColor: Colors.black87,
-          leading: IconButton(
-            icon: Icon(Icons.favorite),
-            color: Colors.red,
-            onPressed: () {},
+          leading: Consumer<Product>(
+            builder: (context, product, _) => IconButton(
+              icon: Icon(
+                  product.isFavorite ? Icons.favorite : Icons.favorite_border),
+              color: Colors.red,
+              onPressed: () {
+                product.toggleFavoriteStatus();
+              },
+            ),
           ),
           title: Text(
-            title,
+            product.title,
             textAlign: TextAlign.center,
             style: const TextStyle(fontSize: 12),
           ),
